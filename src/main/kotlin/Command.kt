34531ff2b09f39500917174cff512e3d3745cbc2
Rigidity <me@rigidnetwork.com>
2020-04-10 14:45:04 -0700

    Setup commands.

--- src/main/kotlin/Command.kt
+++ src/main/kotlin/Command.kt
@@ -1,6 +1,8 @@
+import org.javacord.api.entity.message.Message
+
 class Command(
     val name: String,
     val desc: String,
     val usage: String,
-    val func: (args: Array<String>) -> Boolean
+    val func: (List<String>, Message) -> Boolean
 ) {}
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,8 +1,17 @@
 import java.io.File
+import org.javacord.api.entity.channel.*
+import org.javacord.api.entity.message.Message
 
 fun readFile(name: String): String {
 	return File(name).readText(Charsets.UTF_8)
 }
 fun writeFile(name: String, text: String) {
 	File(name).writeText(text)
+}
+
+fun textChannel(message: Message): ServerTextChannel {
+    return message.getChannel().asServerTextChannel().get()
+}
+fun replyText(channel: ServerTextChannel, text: String) {
+    channel.sendMessage(text)
 }
