8cb46f532c0181b3ef4e93b1fc8d1e728dd08c09
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2020-04-10 20:37:57 -0400

    feat(ktlint) Add formatting for the code

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,73 +1,76 @@
+import java.awt.Color
 import org.javacord.api.DiscordApi
 import org.javacord.api.DiscordApiBuilder
-import org.javacord.api.entity.message.Message
 import org.javacord.api.entity.channel.*
-import java.awt.Color
 
-val commands = arrayListOf<Command>();
+val commands = arrayListOf<Command>()
 
 fun main() {
-	println("Setting up...")
-	loadConfig()
-	configHandler()
-	commands.add(Command("help", "Shows this page.", listOf("help"), { args, message ->
-		if (args.size != 0) {
-			false
-		} else {
-			val list = arrayListOf<String>()
-			for (command in commands) {
-				list.add("** - " + command.name + ":** " + command.desc)
-			}
-			replyPage(message, "***Commands***", list, Color(255, 0, 255))
-			true
-		}
-	}))
-	commands.add(Command("eval", "Evaluates JavaScript code.", listOf("eval [code...]"), { args, message ->
-		if (args.size == 0) {
-			false
-		} else {
-			(Thread {
-				try {
-					replyText(textChannel(message), "Result: " + evalTimed(joinList(args, " "), 1000L))
-				} catch(e: Throwable) {
-					replyText(textChannel(message), "Error: " + e)
-				}
-			}).start()
-			true
-		}
-	}))
-	commands.add(Command("ping", "Says pong.", listOf("ping"), { args, message ->
-		if (args.size != 0) {
-			false
-		} else {
-			replyText(textChannel(message), "Pong!")
-			true
-		}
-	}))
-	commands.add(Command("pong", "Says ping.", listOf("pong"), { args, message ->
-		if (args.size != 0) {
-			false
-		} else {
-			replyText(textChannel(message), "Ping!")
-			true
-		}
-	}))
-	val api: DiscordApi = DiscordApiBuilder().setToken(readFile(".env")).login().join()
-	api.addMessageCreateListener { e ->
-		val message = e.getMessage()
-		val content = message.getContent()
-		val symbol = "$"
-		if (!message.getAuthor().isBotUser() && content.startsWith(symbol) && message.getChannel() is ServerTextChannel) {
-			val components = content.substring(symbol.length).split(" ")
-			val name = components[0]
-			val args = components.drop(1)
-			for (command in commands) {
-				if (name == command.name) {
-					if (!command.func(args, message)) {
-						replyPage(message, "***Usages***", command.usage, Color.ORANGE)
-					}
-				}
-			}
-		}
-	}
+    println("Setting up...")
+    loadConfig()
+    configHandler()
+    commands.add(Command("help", "Shows this page.", listOf("help"),
+    { args, message ->
+              if (args.size != 0) {
+          false
+              } else {
+          val list = arrayListOf<String>()
+          for (command in commands) {
+                      list.add("** - " + command.name + ":** " + command.desc)
+          }
+          replyPage(message, "***Commands***", list, Color(255, 0, 255))
+          true
+              }
+    }))
+    commands.add(Command("eval", "Evaluates JavaScript code.", listOf("eval [code...]"),
+    { args, message ->
+              if (args.size == 0) {
+          false
+              } else {
+          (Thread {
+                       try {
+               replyText(textChannel(message), "Result: " + evalTimed(joinList(args, " "), 1000L))
+                       } catch (e: Throwable) {
+               replyText(textChannel(message), "Error: " + e)
+                       }
+          }).start()
+          true
+              }
+    }))
+    commands.add(Command("ping", "Says pong.", listOf("ping"),
+    { args, message ->
+              if (args.size != 0) {
+          false
+              } else {
+          replyText(textChannel(message), "Pong!")
+          true
+              }
+    }))
+    commands.add(Command("pong", "Says ping.", listOf("pong"),
+    { args, message ->
+              if (args.size != 0) {
+          false
+              } else {
+          replyText(textChannel(message), "Ping!")
+          true
+              }
+    }))
+    val api: DiscordApi = DiscordApiBuilder().setToken(readFile(".env")).login().join()
+    api.addMessageCreateListener { e ->
+        val message = e.getMessage()
+        val content = message.getContent()
+        val symbol = "$"
+        if (!message.getAuthor().isBotUser() && content.startsWith(symbol) && message.getChannel() is ServerTextChannel) {
+            val components = content.substring(symbol.length).split(" ")
+            val name = components[0]
+            val args = components.drop(1)
+            for (command in commands) {
+                if (name == command.name) {
+                    if (!command.func(args, message)) {
+                        replyPage(message, "***Usages***", command.usage, Color.ORANGE)
+                    }
+                }
+            }
+        }
+    }
 }
--- src/main/kotlin/Command.kt
+++ src/main/kotlin/Command.kt
@@ -1,8 +1,8 @@
 import org.javacord.api.entity.message.Message
 
 class Command(
-	val name: String,
-	val desc: String,
-	val usage: List<String>,
-	val func: (List<String>, Message) -> Boolean
-) {}
+    val name: String,
+    val desc: String,
+    val usage: List<String>,
+    val func: (List<String>, Message) -> Boolean
+)
